3b1841db2974b18ed8a53a3f8e0737f7320c795a
AqingCyan <xuekaiqi@creatshare.com>
2019-03-11 20:55:15 +0800

    :tada:react开始

---
+++ todolist/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TodoList from './TodoList';
+// JSX语法中，如果我们要使用自己创建的组件，组件开头以大写字母
+ReactDOM.render(<TodoList />, document.getElementById('root'));
