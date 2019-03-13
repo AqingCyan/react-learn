468377215d1a8efdf9940e0467e2c8da785da7c1
AqingCyan <xuekaiqi@creatshare.com>
2019-03-13 21:25:38 +0800

    :construction:Redux管理起来

--- todolist_antd/src/store/reducer.js
+++ todolist_antd/src/store/reducer.js
@@ -1,16 +1,30 @@
+import {CHANGE_INPUT_VALUE, ADD_TODO_ITEM, DELETE_TODO_ITEM} from './actionTypes';
+
 const defaultState = {
-  inputValue: '学Webpack',
-  list: ['吃饭', '学React']
+  inputValue: '',
+  list: []
 };
 
 // state指的是图书的借还记录本（数据的状态）中的数据
 // action指store收到的话，它得查看借还记录本来操作数据，又把话传过来
 export default (state = defaultState, action) => {
-  if (action.type === 'change_input_value') {
+  if (action.type === CHANGE_INPUT_VALUE) {
     const newState = JSON.parse(JSON.stringify(state)); // reducer可以接收state，但绝不能修改state
     newState.inputValue = action.value;
     return newState;
   }
-  console.log(state, action);
+  if (action.type === ADD_TODO_ITEM) {
+    const newState = JSON.parse(JSON.stringify(state));
+    newState.list.push(newState.inputValue);
+    newState.inputValue = '';
+    console.log(newState);
+    return newState;
+  }
+  if (action.type === DELETE_TODO_ITEM) {
+    const newState = JSON.parse(JSON.stringify(state));
+    newState.list.splice(action.index, 1);
+    return newState;
+  }
+  // console.log(state, action);
   return state
 }
