21e1d68cc7344ad449037b32f48e9b1f82532da1
AqingCyan <xuekaiqi@creatshare.com>
2019-03-20 00:36:49 +0800

    :construction:文档修复

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,13 +3,11 @@ module.exports = {
   description: '我的React.js学习笔记与源码讲解',
   repo: 'https://github.com/AqingCyan/react.js-learn',
   head: [
-    ['link', { rel: 'icon', href: '/favicon.ico' }], // 增加一个自定义的 favicon(网页标签的图标)
+    ['link', { rel: 'icon', href: '/favicon.png' }], // 增加一个自定义的 favicon(网页标签的图标)
   ],
   markdown: {
     lineNumbers: true // 代码块显示行号
   },
-  themeConfig: {
-    sidebarDepth: 2,// e'b将同时提取markdown中h2 和 h3 标题，显示在侧边栏上。
-  },
+  themeConfig: {},
   base: '/react.js-learn/'
 }
