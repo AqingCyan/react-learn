6f57477ea18cb5b443d6cffb524a760db59b7e6d
AqingCyan <xuekaiqi@creatshare.com>
2019-03-20 00:07:33 +0800

    :cookie:慢慢来

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   title: 'React学习指南',
   description: '我的React.js学习笔记与源码讲解',
+  repo: 'https://github.com/AqingCyan/react.js-learn',
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }], // 增加一个自定义的 favicon(网页标签的图标)
   ],
@@ -10,4 +11,5 @@ module.exports = {
   themeConfig: {
     sidebarDepth: 2,// e'b将同时提取markdown中h2 和 h3 标题，显示在侧边栏上。
   },
+  base: '/react.js-learn/'
 }
