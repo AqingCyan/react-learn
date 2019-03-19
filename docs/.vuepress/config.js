5f1e698257ddb83fa853958d04a29b6f281712d3
AqingCyan <xuekaiqi@creatshare.com>
2019-03-19 23:24:24 +0800

    :tada:电子书

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  title: 'React学习指南',
+  description: '我的React.js学习笔记与源码讲解',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }], // 增加一个自定义的 favicon(网页标签的图标)
+  ],
+}
