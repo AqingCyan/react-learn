0ed7db942cb1efad0afcf7e884c6d2608c0d8c2c
AqingCyan <xuekaiqi@creatshare.com>
2019-08-13 02:43:57 +0800

    refer(all codes): 删除陈旧代码进行重新学习

--- todolist_antd/src/store/index.js
+++
@@ -1,7 +0,0 @@
-import {createStore} from 'redux';
-import reducer from './reducer';
-
-// 把图书借还记录本的数据传给图书管理员，管理员就知道数据状态了
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
-export default store;
